7d5d9871a0b88f864cc44889d2f168ff050b5588
thaitwo <tmtu90@gmail.com>
2016-09-08 14:44:45 -0700

    Updated templates files

--- routes/index.js
+++ routes/index.js
@@ -16,4 +16,24 @@ router.get('/contact', function(req, res, next) {
 	res.render('contact', { title: 'CONTACT'});
 });
 
+/* GET gallery page */
+router.get('/gallery', function(req, res, next) {
+	res.render('gallery', { title: 'GALLERIES'});
+});
+
+/* GET gallery landscape page */
+router.get('/gallery/landscape', function(req, res, next) {
+	res.render('gallery-landscape', { title: 'LANDSCAPE'});
+});
+
+/* GET gallery portrait page */
+router.get('/gallery/portrait', function(req, res, next) {
+	res.render('gallery-portrait', { title: 'PORTRAIT'});
+});
+
+/* GET gallery food page */
+router.get('/gallery/food', function(req, res, next) {
+	res.render('gallery-food', { title: 'FOOD'});
+});
+
 module.exports = router;
