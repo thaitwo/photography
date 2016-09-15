df313b376ca7ffa8cd522bd5bda2fec8853c3aab
thaitwo <tmtu90@gmail.com>
2016-09-15 11:44:08 -0700

    converted indentation to spaces for files

--- routes/index.js
+++ routes/index.js
@@ -3,37 +3,37 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Thaitwo' });
+  res.render('index', { title: 'Thaitwo' });
 });
 
 /* GET about page */
 router.get('/about', function(req, res, next) {
-	res.render('about', { title: 'About'});
+  res.render('about', { title: 'About'});
 });
 
 /* GET contact page */
 router.get('/contact', function(req, res, next) {
-	res.render('contact', { title: 'Contact'});
+  res.render('contact', { title: 'Contact'});
 });
 
 /* GET gallery page */
 router.get('/gallery', function(req, res, next) {
-	res.render('gallery', { title: 'Galleries'});
+  res.render('gallery', { title: 'Galleries'});
 });
 
 /* GET gallery landscape page */
 router.get('/gallery/landscape', function(req, res, next) {
-	res.render('gallery-landscape', { title: 'Landscape'});
+  res.render('gallery-landscape', { title: 'Landscape'});
 });
 
 /* GET gallery portrait page */
 router.get('/gallery/portrait', function(req, res, next) {
-	res.render('gallery-portrait', { title: 'Portrait'});
+  res.render('gallery-portrait', { title: 'Portrait'});
 });
 
 /* GET gallery food page */
 router.get('/gallery/food', function(req, res, next) {
-	res.render('gallery-food', { title: 'Food'});
+  res.render('gallery-food', { title: 'Food'});
 });
 
 module.exports = router;
