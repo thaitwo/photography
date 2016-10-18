e15ae7761704d147aead50f31f44e9e64fe13214
thaitwo <tmtu90@gmail.com>
2016-10-18 14:19:35 -0700

    styled mobile dropdown menu

--- public/javascripts/top-nav-dropdown.js
+++ public/javascripts/top-nav-dropdown.js
@@ -1,11 +1,14 @@
 var $dropdownMenu = $('.dropdown');
 var $dropdownButton = $('#dropdown-button');
 var $faBars = $('.fa-bars');
+var $heroTitle = $('#hero-title');
 
 function activateDropdown() {
   $dropdownButton.click(function() {
   	$dropdownButton.toggleClass('fa-bars');
   	$dropdownButton.toggleClass('fa-times');
+
+  	$heroTitle.toggle();
 
     $dropdownMenu.slideToggle();
   })
