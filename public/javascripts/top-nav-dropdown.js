dad3fe9e58c9e2cdad54c59c0033f24e0265ccaf
thaitwo <tmtu90@gmail.com>
2016-10-18 11:10:04 -0700

    added meta viewport for mobile responsive

--- public/javascripts/top-nav-dropdown.js
+++ public/javascripts/top-nav-dropdown.js
@@ -1,14 +1,18 @@
 var $dropdownMenu = $('.dropdown');
 var $dropdownButton = $('#dropdown-button');
+var $faBars = $('.fa-bars');
 
 function activateDropdown() {
   $dropdownButton.click(function() {
+  	$dropdownButton.toggleClass('fa-bars');
+  	$dropdownButton.toggleClass('fa-times');
+
     $dropdownMenu.slideToggle();
   })
 };
 
 $(document).ready(function() {
   if($('.dropdown')) {
-    activateDropdown(); 
+    activateDropdown();
   }
-})
+});
